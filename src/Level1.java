043490eec50720a49d21158b4dedf7233fa7ed9a
Otryaskin Andrey <kinlydog@gmail.com>
2022-07-28 05:25:30 +1000

    Update

--- src/Level1.java
+++ src/Level1.java
@@ -11,27 +11,28 @@ public class Level1 {
 
         int day = 1;
         boolean flag = true;
-        int xlen = battle.length - 1;
-        int ylen = battle[0].length - 1;
+        int xlen = battle.length;
+        int ylen = battle[0].length;
 
         while (flag) {
             flag = false;
             day++;
 
-            for (int i = 0; i < battle.length; i++) {
-                for (int j = 0; j < battle[i].length; j++) {
+            for (int i = 0; i < xlen; i++) {
+                for (int j = 0; j < ylen; j++) {
                     if (battle[i][j] == day - 1) {
-                        if (i > 0    && battle[i - 1][j] == 0) battle[i - 1][j] = day;
-                        if (j > 0    && battle[i][j - 1] == 0) battle[i][j - 1] = day;
-                        if (j < ylen && battle[i][j + 1] == 0) battle[i][j + 1] = day;
-                        if (i < xlen && battle[i + 1][j] == 0) battle[i + 1][j] = day;
+                        if (i > 0        && battle[i - 1][j] == 0) battle[i - 1][j] = day;
+                        if (j > 0        && battle[i][j - 1] == 0) battle[i][j - 1] = day;
+                        if (j < ylen - 1 && battle[i][j + 1] == 0) battle[i][j + 1] = day;
+                        if (i < xlen - 1 && battle[i + 1][j] == 0) battle[i + 1][j] = day;
                     }
                 }
             }
 
-            for (int i = 0; i < battle.length; i++) {
-                for (int j = 0; j < battle[0].length; j++) {
-
+            for (int i = 0; i < xlen; i++) {
+                for (int j = 0; j < ylen; j++) {
+                    if (battle[i][j] == 0)
+                        flag = true;
                 }
             }
 
