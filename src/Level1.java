b137e871eed35b2f52ef4319b56ff73b6dc3f13c
Otryaskin Andrey <kinlydog@gmail.com>
2022-07-28 05:18:13 +1000

    Update

--- src/Level1.java
+++ src/Level1.java
@@ -1,21 +1,18 @@
 public class Level1 {
     public static int ConquestCampaign(int n, int m, int l, int[] battalion) {
-        int day = 1;
-        int x;
-        int y;
-        boolean flag = true;
-
         int[][] battle = new int[n][m];
-        int xlen = battle.length - 1;
-        int ylen = battle[0].length - 1;
 
-        // Start point, day 1
         for (int i = 0; i < l * 2 - 1; i += 2) {
-            x = battalion[i] - 1;
-            y = battalion[i + 1] - 1;
+            int x = battalion[i] - 1;
+            int y = battalion[i + 1] - 1;
 
             battle[x][y] = 1;
         }
+
+        int day = 1;
+        boolean flag = true;
+        int xlen = battle.length - 1;
+        int ylen = battle[0].length - 1;
 
         while (flag) {
             flag = false;
@@ -24,8 +21,8 @@ public class Level1 {
             for (int i = 0; i < battle.length; i++) {
                 for (int j = 0; j < battle[i].length; j++) {
                     if (battle[i][j] == day - 1) {
-                        if (i > 0 && battle[i - 1][j] == 0)    battle[i - 1][j] = day;
-                        if (j > 0 && battle[i][j - 1] == 0)    battle[i][j - 1] = day;
+                        if (i > 0    && battle[i - 1][j] == 0) battle[i - 1][j] = day;
+                        if (j > 0    && battle[i][j - 1] == 0) battle[i][j - 1] = day;
                         if (j < ylen && battle[i][j + 1] == 0) battle[i][j + 1] = day;
                         if (i < xlen && battle[i + 1][j] == 0) battle[i + 1][j] = day;
                     }
@@ -34,8 +31,7 @@ public class Level1 {
 
             for (int i = 0; i < battle.length; i++) {
                 for (int j = 0; j < battle[0].length; j++) {
-                    if (battle[i][j] == 0)
-                        flag = true;
+
                 }
             }
 
