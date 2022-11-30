be1640a2949770454d06dfdbdc833366220d7438
Andrey Otryaskin <kinlydog@gmail.com>
2022-11-30 17:56:45 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -13,25 +13,25 @@ public class Level1 {
         }
 
         boolean battleIsOver = true;
-        int battlefieldDay = battle.length;
+        int battlefieldWidth = battle.length;
         int battlefieldLength = battle[0].length;
 
         while (battleIsOver) {
             battleIsOver = false;
             battlefieldDay++;
 
-            for (int i = 0; i < battlefieldDay; i++) {
+            for (int i = 0; i < battlefieldWidth; i++) {
                 for (int j = 0; j < battlefieldLength; j++) {
                     if (battle[i][j] == battlefieldDay - 1) {
                         if (i > 0 && battle[i - 1][j] == 0) battle[i - 1][j] = battlefieldDay;
                         if (j > 0 && battle[i][j - 1] == 0) battle[i][j - 1] = battlefieldDay;
                         if (j < battlefieldLength - 1 && battle[i][j + 1] == 0) battle[i][j + 1] = battlefieldDay;
-                        if (i < battlefieldDay - 1 && battle[i + 1][j] == 0) battle[i + 1][j] = battlefieldDay;
+                        if (i < battlefieldWidth - 1 && battle[i + 1][j] == 0) battle[i + 1][j] = battlefieldDay;
                     }
                 }
             }
 
-            for (int i = 0; i < battlefieldDay; i++) {
+            for (int i = 0; i < battlefieldWidth; i++) {
                 for (int j = 0; j < battlefieldLength; j++) {
                     if (battle[i][j] == 0) battleIsOver = true;
                 }
